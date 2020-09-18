babb061505689c7bc8e20a2846a17c2b2ec904eb
Melissa C <66797764+Wednesdayw@users.noreply.github.com>
2020-09-18 13:25:44 -0400

    Stage 3 testing testing

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,34 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import BubblePage from "./BubblePage";
+import { fetchApi as mockFetch } from "./fetchApi";
 
-test("Fetches data and renders the bubbles", () => {
+jest.mock("./fetchApi");
+const testData = {
+  data: [
+    {
+      color: "aliceblue",
+      code: {
+        hex: "#f0f8ff",
+      },
+      id: 1,
+    },
+    {
+      color: "limegreen",
+      code: {
+        hex: "#99ddbc",
+      },
+      id: 2,
+    },
+  ],
+};
+
+test("Fetches data and renders the bubbles", async () => {
   // Finish this test
+  mockFetch.mockResolvedValueOnce(testData);
+  const { debug, getByText } = render(<BubblePage />);
+  await waitFor(() => {
+    debug();
+    expect(getByText(/aliceblue/i)).toBeInTheDocument()
+  })
 });
